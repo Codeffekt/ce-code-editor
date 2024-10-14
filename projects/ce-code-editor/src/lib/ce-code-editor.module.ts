e0fa27dd5ab70f9e17a273bfd7990fb8e88c824e
fabien <fabien.pelisson@codeffekt.com>
2024-10-14 12:33:08 +0200

    v15

--- projects/ce-code-editor/src/lib/ce-code-editor.module.ts
+++ projects/ce-code-editor/src/lib/ce-code-editor.module.ts
@@ -4,7 +4,7 @@ import { CeCodeEditorComponent } from './code-editor.component';
 import { FormsModule } from '@angular/forms';
 import { CeCodeMirrorComponent } from './code-mirror/code-mirror.component';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { StringArrayToStringPipe } from './array-to-string.pipe';
 @NgModule({
