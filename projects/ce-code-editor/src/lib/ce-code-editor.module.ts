b376d760120ad9ac2ca1bbf9486d3f1572b0ddbf
fabien <fabien.pelisson@codeffekt.com>
2025-03-14 19:16:07 +0100

    fix material pb

--- projects/ce-code-editor/src/lib/ce-code-editor.module.ts
+++ projects/ce-code-editor/src/lib/ce-code-editor.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { CeCodeEditorComponent } from './code-editor.component';
 import { FormsModule } from '@angular/forms';
 import { CeCodeMirrorComponent } from './code-mirror/code-mirror.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { StringArrayToStringPipe } from './array-to-string.pipe';
 @NgModule({
