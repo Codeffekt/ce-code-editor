7159877598becfc9833a3af30cf602418b87afc2
fabien <fabien.pelisson@codeffekt.com>
2025-07-18 19:44:10 +0200

    fix: library exports and tsconfig paths

--- projects/ce-code-editor/src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ce-code-editor.module';
-export * from './code-editor';
-export * from './code-mirror';
-export * from './code-editor-config';
-export * from './form-object-block';
-export * from './form-editor-dialog';
--- projects/ce-code-editor/src/public-api.ts
+++ projects/ce-code-editor/src/public-api.ts
@@ -1,4 +1,7 @@
-/*
- * Public API Surface of ce-code-editor
- */
-export * from './lib';
+export * from './lib/ce-code-editor.module';
+export * from './lib/code-editor';
+export * from './lib/code-mirror';
+export * from './lib/code-editor-config';
+export * from './lib/form-object-block';
+export * from './lib/form-editor-dialog';
+export * from './lib/array-to-string.pipe';
